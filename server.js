34d25ec05b66c05ee5c49cdb2a002bd294afdb2a
Valdi <valdilaurentius@gmail.com>
2023-06-23 19:02:40 -0700

    setting up connection to database

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 // require express and create a reference variable
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser')
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
 
 //listening to port
 var server = app.listen(3000, () => {
     console.log('server is running on port', server.address().port);
    });
+
+app.use(express.static(__dirname));// tell express.js that we will be using static file
+
+var dbUrl = 'mongodb+srv://valdilaurentius:g2aIIRReH4aGW9gX@cluster0.l9uktyd.mongodb.net/'
+
+mongoose.connect(dbUrl, (err) => {
+    console.log('mongodb connected', err);
+})
+
+var Message = mongoose.model('Message', {name: String, message: String})
