4d8abdf9b2a9b914beb23b224e4e2fab5ba4b755
Valdi <valdilaurentius@gmail.com>
2023-06-25 20:49:23 -0700

    implemented send image feature

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var multer = require('multer');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,6 +14,18 @@ app.use(express.static(__dirname)); // tell express.js that we will be using sta
 
 var dbUrl = 'mongodb+srv://valdilaurentius:g2aIIRReH4aGW9gX@cluster0.l9uktyd.mongodb.net/';
 
+// Configure multer to store uploaded images in the 'uploads' directory
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + '-' + file.originalname);
+    }
+  });
+
+var upload = multer({storage: storage});
+  
 mongoose
   .connect(dbUrl)
   .then(() => {
@@ -23,7 +36,7 @@ mongoose
     console.error('Error connecting to the database:', error);
   });
 
-var Message = mongoose.model('Message', { name: String, message: String, timestamp: { type: Date, default: Date.now } });
+var Message = mongoose.model('Message', { name: String, message: String, timestamp: { type: Date, default: Date.now }, image: String });
 
 app.get('/messages', (req, res) => {
   Message.find({})
@@ -36,19 +49,23 @@ app.get('/messages', (req, res) => {
     });
 });
 
-app.post('/messages', (req, res) => {
-  const message = new Message(req.body);
-  message
-    .save()
-    .then(() => {
-      io.emit('message', message);
-      res.sendStatus(200);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
+app.post('/messages', upload.single('image'), (req, res) => {
+    const message = new Message({
+      name: req.body.name,
+      message: req.body.message,
+      image: req.file ? req.file.path : null
     });
-});
+    message
+      .save()
+      .then(() => {
+        io.emit('message', message);
+        res.sendStatus(200);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  });
 
 app.delete('/messages/:id', (req, res) => {
     const messageId = req.params.id;
