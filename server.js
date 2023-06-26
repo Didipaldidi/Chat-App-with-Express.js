feb7f99af8416d78c670395385d40c189b7ec6e4
Valdi <valdilaurentius@gmail.com>
2023-06-25 18:07:37 -0700

    implemented the send and post message

--- server.js
+++ server.js
@@ -2,22 +2,51 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser')
+var http = require('http').Server(app);
+var io = require('socket.io')(http)
 
 var app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}))
-
-//listening to port
-var server = app.listen(3000, () => {
-    console.log('server is running on port', server.address().port);
-   });
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(__dirname));// tell express.js that we will be using static file
 
 var dbUrl = 'mongodb+srv://valdilaurentius:g2aIIRReH4aGW9gX@cluster0.l9uktyd.mongodb.net/'
 
-mongoose.connect(dbUrl, (err) => {
-    console.log('mongodb connected', err);
-})
+mongoose.connect(dbUrl)
+  .then(() => {
+    console.log('Connected to the database');
+    // Additional code here
+  })
+  .catch((error) => {
+    console.error('Error connecting to the database:', error);
+  });
 
-var Message = mongoose.model('Message', {name: String, message: String})
+var Message = mongoose.model('Message', {name: String, message: String});
+
+app.get('/messages', (req, res) => {
+    Message.find({})
+      .then((messages) => {
+        res.send(messages);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+      });
+  });
+  
+  app.post('/messages', (req, res) => {
+    const message = new Message(req.body);
+    message.save()
+      .then(() => {
+        res.sendStatus(200);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  });
+
+  const server = app.listen(3000, () => {
+    console.log('Server is running on port', server.address().port);
+  });
